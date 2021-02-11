9a552e5a307622d75720969538c4be18794a48a4
Anthony Ruiz <anthonyra95@hotmail.com>
2021-02-10 21:24:29 -0500

    commit

--- HelloKotlin/src/main/kotlin/Qiuz.kt
+++ HelloKotlin/src/main/kotlin/Qiuz.kt
@@ -3,9 +3,15 @@ fun main(args: Array<String>){
     print(test)
 }
 
+fun isWalkingWeather( mood :String, weather: String) = mood == "happy" && weather == "Sunny"
+fun isLazyWeather(mood:String, weather: String,temperature: Int ) = mood == "sad" && weather == "rainy" && temperature == 0
+fun isSwimmingWeahter( temperature: Int) = temperature > 35
 fun whatShouldIDoToday(mood: String, weather:String ="sunny", temperature: Int = 24) : String{
     return when  {
-        mood == "happy" && weather == "Sunny" -> "go for a walk"
+
+        isWalkingWeather(mood, weather) -> "go for a walk"
+        isLazyWeather(mood, weather, temperature) -> "stay in bed"
+        isSwimmingWeahter(temperature) -> "go swimming"
         else -> "Stay home and read."
     }
 }
