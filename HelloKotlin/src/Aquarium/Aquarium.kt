f9ae133c30e28affdc5a29b0f25929652f5647ae
Anthony Ruiz <anthonyra95@hotmail.com>
2021-02-11 20:28:29 -0500

    commit

--- HelloKotlin/src/Aquarium/Aquarium.kt
+++ HelloKotlin/src/Aquarium/Aquarium.kt
@@ -1,6 +1,8 @@
-class Aquarium{
+import sun.security.util.Length
 
-    var width: Int = 20
-    var height: Int =40
-    var length: Int = 100
+class Aquarium( var length: Int =100, var width: Int = 40, var height: Int = 20){
+
+    var volume : Int
+        get() = width * height * length
+        set(value) {height = value /width * height}
 }
