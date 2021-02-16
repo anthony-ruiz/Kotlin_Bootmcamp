98273f32b95d0118ea7737ce50a641af1fb19a13
Anthony Ruiz <anthonyra95@hotmail.com>
2021-02-16 18:32:55 -0500

    commit

--- HelloKotlin/src/Books/Book.kt
+++ HelloKotlin/src/Books/Book.kt
@@ -1,9 +1,10 @@
+import kotlin.random.Random
 
 const val MAX_BOOKS_BORROWED = 20
 
 
 
-class Book(var title: String, var author: String, var year : Int){
+class Book(var title: String, var author: String, var year : Int, var pages :Int){
 
 
     fun canBorrow(booksBorrowed :Int) : Boolean{
@@ -20,9 +21,23 @@ class Book(var title: String, var author: String, var year : Int){
 
 }
 
+fun Book.weight(book :Book)  = book.pages * 1.5
+
+fun Book.tornPages(book : Book, pages: Int) {
+    book.pages -= pages
+    println("pages torn: $pages")
+}
+
+class Puppy{
+    fun playWithBook(book: Book) = book.tornPages(book, Random.nextInt(book.pages))
+}
+
 fun main(args : Array<String>){
-    val myBook = Book("Hamlet" , "Shakespeare" , 1601)
-    myBook.printURL()
+    val myBook = Book("Hamlet","Shakespeare", 1601,205)
+    println("initial pages: ${myBook.pages}")
+    val myPuppy = Puppy()
+    myPuppy.playWithBook(myBook)
+    println("book ended with: ${myBook.pages}")
 
 
 }
